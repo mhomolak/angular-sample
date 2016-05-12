3ba5ec5721864a02864bb8b4998a299ef19896a0
Mike Homolak <mhomolak@yahoo.com>
2016-05-12 14:35:32 -0700

    users can signup via angular

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -2,7 +2,25 @@
   'use strict';
 
   angular.module('app')
-    .controller('SignupController', function () {
+    .controller('SignupController', function ($scope, $http, $window, $location,
+    $rootScope) {
+
+      $scope.createUser = function() {
+
+        $http.post('/api/v1/users/signup', $scope.user)
+          .then(function (response) {
+            $rootScope.user = response.data;
+            $window.localStorage.setItem('token', response.data.token);
+            $location.path('/');
+        })
+
+        // √ $http post /api/v1/users/signup
+        // √ then
+          // store token
+          // redirect
+        // catch
+          //
+      }
 
     })
 
